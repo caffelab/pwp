16efbd4b06b6f8c0f1eec15f9a5170792a370f7f
Artherson <artherson@Arthersons-MBP.lan>
2019-09-03 17:49:32 +0800

    初始化

--- src/Config.php
+++ src/Config.php
@@ -14,10 +14,17 @@ class Config {
     private function __construct(){
         $path = ROOT_PATH.'/app/config/config.php';
         $this->config = include($path);
+        $this->loadFunc();
     }
 
     static public function getConfig(){
         $config  = new Config();
         return $config->config;
+    }
+
+    public function loadFunc(){
+        if(is_file(__DIR__.'/Function.php')){
+            require_once(__DIR__.'/Function.php');
+        }
     }
 }
