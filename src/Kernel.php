238719f995435c51c5e4f2f94e7044a2f3fbc832
Artherson <artherson@Arthersons-MBP.lan>
2019-09-06 11:14:39 +0800

    初始化

--- src/Kernel.php
+++ src/Kernel.php
@@ -4,18 +4,25 @@ namespace pwp;
 
 class Kernel
 {
-    /**
-     * Class constructor.
-     */
-    public function __construct()
-    {
-        $this->autoload();
-        return true;
-    }
-    //自动加载
-    protected function autoload(){
-        spl_autoload_register(__CLASS__,'loadClass');
+    static public function run(){
+
     }
 
-
+    protected function router(){
+        $url = $_SERVER['REQUEST_URI'];
+        $url = trim($url,'/');
+        $arr = explode('/',$url);
+        if(!defined('APP_NAME')){
+            define('APP_NAME','app');
+        }
+        $module_name = $arr[0];
+        $layer_name = $arr[1];
+        $role_name = $arr[2];
+        $action_name = $arr[3];
+        //LAYOUT 层名
+        //MODULE 模块名
+        //ACTION  函数名称
+        //APPNAME 类名称
+        //目录为  app\module\layout\appname.php/action
+    }
 }
